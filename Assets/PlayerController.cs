37029cf44503f5ca6ac778e2889a775c53eb1d74
quomitter <109629602+quomitter@users.noreply.github.com>
2022-08-20 06:40:23 -0400

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+        rb.rotation = Mathf.Rad2Deg * Mathf.Atan2(Input.GetAxis("RVertical"),Input.GetAxis("RHorizontal"));
+    }
+}
