1d927553b7db4f403989a2357ed7a4c5806a4a10
quomitter <109629602+quomitter@users.noreply.github.com>
2022-08-20 18:45:44 -0400

    aaahh

--- Assets/FirePoint.cs
+++ Assets/FirePoint.cs
@@ -41,8 +41,8 @@ public class FirePoint : MonoBehaviour
 
         if (Time.time > fireRate + lastShot && Input.GetButtonDown("Fire1"))
         {
-            GameObject clone = Instantiate(bullet1, firePoint.transform.position, firePoint.transform.rotation * Quaternion.Euler(0, 0, 90f));
-            // Physics2D.IgnoreCollision(clone.GetComponent<Collider2D>(), playership.GetComponent<Collider2D>());
+            GameObject clone = Instantiate(bullet1, firePoint.transform.position, firePoint.transform.rotation);
+            //Physics2D.IgnoreCollision(clone.GetComponent<Collider2D>(), playership.GetComponent<Collider2D>());
             shooter = clone.GetComponent<Rigidbody2D>();
             shooter.AddForce(new Vector2(1 * Mathf.Sin(player1.rotation), 1 * -Mathf.Cos(player1.rotation)) * bulletSpeed, ForceMode2D.Impulse);
             Debug.Log(player1.rotation);
